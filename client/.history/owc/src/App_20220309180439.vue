209127dd5c9abf28a87e9eafcb72a034f5a488f6
Marco Sciacovelli <marco.sciacovelli@gmail.com>
2022-03-09 18:13:05 +0100

    Created vue project

---
+++ client/.history/owc/src/App_20220309180439.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-app-bar
+      dense
+      dark
+      color="primary"
+    >
+      
+    </v-app-bar>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ client/.history/owc/src/App_20220309181117.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-app>
+    <v-app-bar
+      class="app-bar"
+      color="primary"
+      dense
+      dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      bottom
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-title>Foo</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Bar</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Fizz</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Buzz</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+      drawer: false,
+      group: null,
+    }),
+
+    watch: {
+      group () {
+        this.drawer = false
+      },
+    },
+};
+</script>
+
+<style>
+
+  .app-bar {
+    max-height: 50px;
+  }
+</style>
