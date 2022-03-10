a40670d7d1760f620a7466c748f56574d82581ea
daniel planötscher <dplanoetscher@unibz.it>
2022-03-10 09:50:58 +0100

    bugfix

--- client/src/cache/apiClient.js
+++ client/src/cache/apiClient.js
@@ -29,7 +29,7 @@ export default class ApiClient {
             try {
                 const result = await fetch(`${this.baseUrl}${path}`, {
                     method: 'POST',
-                    body: payload
+                    body: JSON.stringify(payload)
                 });
                 if (result.ok) {
                     const data = await result.json();
@@ -48,7 +48,7 @@ export default class ApiClient {
             try {
                 const result = await fetch(`${this.baseUrl}${path}`, {
                     method: 'PUT',
-                    body: payload
+                    body: JSON.stringify(payload)
                 });
                 if (result.ok) {
                     const data = await result.json();
